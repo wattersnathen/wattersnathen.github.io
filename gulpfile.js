49f35b3f57af9bf92f5257f0fda27e50c2729318
Nathen Watters <wattersnathen@gmail.com>
2015-08-05 18:33:11 -0600

    initial gulp file

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function () {});
+
+gulp.task('styles', function () {});
+
+gulp.task('watch', function () {});
+
+gulp.task('default', function () {});
