cd54f0e9ae407444debe4efb633f26a81387abd1
Nathen Watters <wattersnathen@gmail.com>
2015-08-05 18:39:23 -0600

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,30 @@ var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 
-gulp.task('scripts', function () {});
+gulp.task('scripts', function () {
+	return gulp.src('./site/*.js')
+		.pipe(concat('app.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./build/'));
+});
 
-gulp.task('styles', function () {});
+gulp.task('styles', function () {
+	return gulp.src('./site/*.css')
+		.pipe(concat('app.css'))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('./build'));
+});
 
-gulp.task('watch', function () {});
+gulp.task('html', function () {
+	return gulp.src('./build.html')
+		.pipe(minifyHtml())
+		.pipe(gulp.dest('./build/'));
+});
 
-gulp.task('default', function () {});
+gulp.task('watch', function () {
+	gulp.watch('./site/*.css', ['styles']);
+	gulp.watch('./site/*.js', ['scripts']);
+	gulp.watch('./build.html', ['html']);
+});
+
+gulp.task('default', ['scripts', 'styles', 'html', 'watch']);
