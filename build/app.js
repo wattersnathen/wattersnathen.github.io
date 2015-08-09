6cb09134cb922807ec76118db23a36e0c7382bd6
Nathen Watters <wattersnathen@gmail.com>
2015-08-08 23:41:52 -0600

    removed console statement

--- build/app.js
+++ build/app.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){var e=$(".project .projectDetails p")[3];e.innerHTML='<p style="color: red; font-weight: 900; font-size: 14px; text-align: center;">This app is currently under construction. Inquire about progress using the contact form below. Thanks!</p>',console.log(e)});
+$(document).ready(function(){var e=$(".project .projectDetails p")[3];e.innerHTML='<p style="color: red; font-weight: 900; font-size: 14px; text-align: center;">This app is currently under construction. Inquire about progress using the contact form below. Thanks!</p>'});
--- site/main.js
+++ site/main.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
 	var neighborhoodMap = $('.project .projectDetails p')[3];
-
-
 	neighborhoodMap.innerHTML = '<p style="color: red; font-weight: 900; font-size: 14px; text-align: center;">This app is currently under construction. Inquire about progress using the contact form below. Thanks!</p>';
-
-	console.log(neighborhoodMap);
 });
