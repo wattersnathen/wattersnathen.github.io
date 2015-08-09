5e1cab2545273e60a64faa292df3cf44dec29bff
Nathen Watters <wattersnathen@gmail.com>
2015-08-09 16:00:23 -0600

    Using JavaScript to resize .siteHeader and its content based on viewport height

--- site/main.js
+++ site/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
 	var neighborhoodMap = $('.project .projectDetails p')[3];
 	neighborhoodMap.innerHTML = '<p style="color: red; font-weight: 900; font-size: 14px; text-align: center;">This app is currently under construction. Inquire about progress using the contact form below. Thanks!</p>';
+
+	resizeHeader();
 });
+
+window.onresize = function (e) {
+	resizeHeader();
+};
+
+var resizeHeader = function () {
+	var viewportHeight = window.innerHeight,
+		siteHeader = document.getElementsByClassName('siteHeader')[0];
+
+	siteHeader.style.height = viewportHeight + 'px';
+
+	if (viewportHeight > 580) {
+		siteHeader.style.paddingTop = (viewportHeight * 0.3) + 'px';
+	} else {
+		siteHeader.style.paddingTop = (viewportHeight * 0.2) + 'px';
+	}
+};
